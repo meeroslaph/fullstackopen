19cb857f35241e640a2edb519757f4b83d5c0d9b
meeroslaph <6173298+meeroslaph@users.noreply.github.com>
2025-03-20 23:43:23 +0100

    Add Statistics component to encapsulate metrics

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,6 +8,23 @@ const Button = ({ onClick, text }) => {
   return <button onClick={onClick}>{text}</button>;
 };
 
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = all > 0 ? (good - bad) / all : 0;
+  const positive = all > 0 ? (good / all) * 100 : 0;
+
+  return (
+    <>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>All: {all}</p>
+      <p>Average: {average}</p>
+      <p>Positive: {positive} %</p>
+    </>
+  );
+};
+
 const App = () => {
   const title = "Give Feedback";
   const statisticsSection = "Statistics";
@@ -16,10 +33,6 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const all = good + neutral + bad;
-  const average = all > 0 ? (good - bad) / all : 0;
-  const positive = all > 0 ? (good / all) * 100 : 0;
-
   return (
     <div>
       <Header text={title} />
@@ -27,12 +40,7 @@ const App = () => {
       <Button onClick={() => setNeutral(neutral + 1)} text="Neutral" />
       <Button onClick={() => setBad(bad + 1)} text="Bad" />
       <Header text={statisticsSection} />
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>All: {all}</p>
-      <p>Average: {average}</p>
-      <p>Positive: {positive} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
