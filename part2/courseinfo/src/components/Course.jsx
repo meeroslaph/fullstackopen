fd943839601c776517b7ae524f68f29226afce5b
meeroslaph <6173298+meeroslaph@users.noreply.github.com>
2025-04-02 12:27:11 +0200

    Add part2a exercises

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,44 @@
+const Header = (props) => {
+  return <h2>{props.course.name}</h2>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  return (
+    <p>
+      <b>
+        Total of {parts.reduce((sum, part) => sum + part.exercises, 0)}{" "}
+        exercises
+      </b>
+    </p>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default Course;
