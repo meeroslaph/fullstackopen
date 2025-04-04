f6cacb5e73cd818934736ae66676643af091648e
meeroslaph <6173298+meeroslaph@users.noreply.github.com>
2025-04-04 20:21:52 +0200

    Exercise 2.9: Refactor phonebook app to include search functionality and ensure unique entries

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,22 +2,41 @@ import { useState } from "react";
 import Person from "./components/Person";
 
 const App = () => {
+  const [filter, setFilter] = useState("");
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", phone: "040-1234567" },
+    { name: "Arto Hellas", phone: "040-123456", id: 1 },
   ]);
   const [newName, setNewName] = useState("");
   const [newPhone, setNewPhone] = useState("");
 
+  const personsToShow = filter
+    ? persons.filter((person) =>
+        person.name.toLowerCase().includes(filter.toLowerCase())
+      )
+    : persons;
+
   const addPerson = (event) => {
     event.preventDefault();
-    if (persons.some((person) => person.name === newName)) {
+    if (
+      persons.some(
+        (person) => person.name.toLowerCase() === newName.toLowerCase()
+      )
+    ) {
       alert(`${newName} is already added to the phonebook`);
       return;
     }
-    const personObject = { name: newName, phone: newPhone };
+    const personObject = {
+      name: newName,
+      phone: newPhone,
+      id: persons.length + 1,
+    };
     setPersons(persons.concat(personObject));
     setNewName("");
     setNewPhone("");
+  };
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
   };
 
   const handleNameChange = (event) => {
@@ -31,6 +50,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown with:{" "}
+        <input value={filter} onChange={handleFilterChange} />
+      </div>
+      <h2>Add new contact</h2>
       <form onSubmit={addPerson}>
         <div>
           Name: <input value={newName} onChange={handleNameChange} />
@@ -43,8 +67,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <Person key={person.name} person={person} />
+      {personsToShow.map((person) => (
+        <Person key={person.id} person={person} />
       ))}
     </div>
   );
